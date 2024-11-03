aebbb5ca52e2ac6573d2d2ec5169b9e79433a214
Moisso Bat <utulisateurmacedonien@gmail.com>
2024-11-03 20:08:51 +0000

    1-Function that check for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *check on the c variable
+ *if digit return 1 else 0
+ */
+
+int _isdigit(int c)
+	{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+	
+		return (0);
+	}
+	}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-
 #include "main.h"
 #include <stdio.h>
 
-
 int _isupper(int c);
-
+int _isdigit(int c);
 #endif
