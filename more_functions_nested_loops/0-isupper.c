d8ed43f2b4f1d84caa69dbb4108413602ba569a6
Moisso Bat <utulisateurmacedonien@gmail.com>
2024-11-03 20:22:16 +0000

    0-Return a 1 if there is an uppercase, else a 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,20 @@
 #include "main.h"
+/**
+ * _isupper - check the character in c
+ * @c: character being check)
+ *
+ * Return: 1 if uppercase, else 0
+ */
 
-	/**
-	*Write a function that checks for uppercase character.
-	*
-	*  Prototype: int _isupper(int c);
-	*  Returns 1 if c is uppercase
-	*  Returns 0 otherwise
-	*
-	*/
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	return (1);
+	{
+		return (1);
+	}
 	else
+	{
 	return (0);
 	}
+}
